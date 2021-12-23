ba0f54dee82c96dd05405215d4740912df824f58
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-12-23 00:28:59 +0000

    nova assert

--- Nova/log/log.cpp
+++ Nova/log/log.cpp
@@ -3,18 +3,30 @@
 
 #include <iostream>
 
+#ifdef nova_assert
+#include <intrin.h>
+#endif // nova_assert
+
 namespace Nova::Bark {
 
 	void Initialize() {
-		NOVA_BARK_INFO("Nova Bark Initialized");
+		nova_bark_info("Nova Bark Initialized");
 	}
 	void Terminate() {
-		NOVA_BARK_INFO("Nova Bark Terminated");
+		nova_bark_info("Nova Bark Terminated");
 	}
 
-	void submit(Level level, const std::string& msg) {
+	void submit(const Level level, const std::string& msg) {
 		static constexpr std::string_view levels[static_cast<char>(Level::MAX)] = {"Debug", "Info", "Warn", "Error", "Fatal"};
 		(level < Level::Error ? std::cout : std::cerr) << levels[static_cast<char>(level)] << ": " << msg << std::endl;
 	}
+
+	#ifdef nova_assert
+	void assertion(bool condition, const std::string_view msg, const std::string_view file, const int line) {
+		if (condition) ; else {
+			nova_bark_fatal("{}@{}\nAssertion: {}", file, line, msg);
+		}
+	}
+	#endif // nova_assert
 
 }
--- Nova/log/log.h
+++ Nova/log/log.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "defines.h"
-#include <initializer_list>
+#include <format>
 
-#define NOVA_BARK_STATE_WARN
-#define NOVA_BARK_STATE_INFO
-#define NOVA_BARK_STATE_DEBUG
+#define __N_OVA_BARK_STATE_WARN
+#define __N_OVA_BARK_STATE_INFO
+#define __N_OVA_BARK_STATE_DEBUG
 
 #ifdef NOVA_RELEASE
-#undef NOVA_BARK_STATE_DEBUG
-#endif
+#undef __N_OVA_BARK_STATE_DEBUG
+#endif // NOVA_RELEASE
 
-#include <format>
+#ifdef NOVA_DEBUG
+#define nova_assert(cond, msg) ::Nova::Bark::assertion(cond, msg, __FILE__, __LINE__)
+#endif // NOVA_DEBUG
 
 namespace Nova::Bark {
 	enum class Level : char {
@@ -24,31 +26,35 @@ namespace Nova::Bark {
 	NOVAPI void Initialize();
 	NOVAPI void Terminate();
 
-	NOVAPI void submit(Level level, const std::string& msg);
+	NOVAPI void submit(const Level level, const std::string& msg);
 
 	template<typename ...Args>
-	void report(Level level, std::string_view msg, Args&&... args) {
+	void report(const Level level, const std::string_view msg, Args&&... args) {
 		return submit(level, std::format(msg, args...));
 	}
+
+	#ifdef nova_assert
+	NOVAPI void assertion(bool condition, const std::string_view msg, const std::string_view file, const int line);
+	#endif // nova_assert
 }
 
-#define NOVA_BARK_FATAL(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Fatal, message, ##__VA_ARGS__)
-#define NOVA_BARK_ERROR(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Error, message, ##__VA_ARGS__)
+#define nova_bark_fatal(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Fatal, message, ##__VA_ARGS__); __debugbreak()
+#define nova_bark_error(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Error, message, ##__VA_ARGS__)
 
-#ifdef NOVA_BARK_STATE_WARN
-#define NOVA_BARK_WARN(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Warn, message, ##__VA_ARGS__)
+#ifdef __N_OVA_BARK_STATE_WARN
+#define nova_bark_warn(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Warn, message, ##__VA_ARGS__)
 #else
-#define NOVA_BARK_WARN(message, ...)
-#endif
+#define nova_bark_warn(message, ...)
+#endif // __N_OVA_BARK_STATE_WARN
 
-#ifdef NOVA_BARK_STATE_WARN
-#define NOVA_BARK_INFO(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Info, message, ##__VA_ARGS__)
+#ifdef __N_OVA_BARK_STATE_INFO
+#define nova_bark_info(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Info, message, ##__VA_ARGS__)
 #else
-#define NOVA_BARK_INFO(message, ...)
-#endif
+#define nova_bark_info(message, ...)
+#endif // __N_OVA_BARK_STATE_INFO
 
-#ifdef NOVA_BARK_STATE_WARN
-#define NOVA_BARK_DEBUG(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Debug, message, ##__VA_ARGS__)
+#ifdef __N_OVA_BARK_STATE_DEBUG
+#define nova_bark_debug(message, ...) ::Nova::Bark::report(::Nova::Bark::Level::Debug, message, ##__VA_ARGS__)
 #else
-#define NOVA_BARK_DEBUG(message, ...)
-#endif
+#define nova_bark_debug(message, ...)
+#endif // __N_OVA_BARK_STATE_DEBUG
