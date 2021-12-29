e4b52470361883f4013dd7605190a471c00d3e27
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-12-29 19:21:29 +0000

    helper class to observe contructors

---
+++ Nova/meta/test.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "log/log.h"
+
+namespace Nova::meta {
+
+	struct Test {
+		int x;
+		Test() : x(0) {
+			nova_bark_debug("Constructor Default");
+		}
+		Test(const int x) : x(x) {
+			nova_bark_debug("Constructor {}", x);
+		}
+		Test(const Test& other) : x(other.x) {
+			nova_bark_debug("Copy Constructor {}", x);
+		}
+		Test& operator=(const Test& other) {
+			x = other.x; nova_bark_debug("Copy Assignment {}", x); return *this;
+		}
+		Test(Test&& other) : x(std::move(other.x)) {
+			other.x = 0; nova_bark_debug("Move Contstructor {}", x);
+		}
+		Test& operator=(Test&& other) {
+			x = std::move(other.x); other.x = 0; nova_bark_debug("Move Assignmet {}", x); return *this;
+		}
+		~Test() {
+			nova_bark_debug("Destructor {}", x);
+		}
+	};
+
+
+}
