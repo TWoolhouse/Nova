de5461746b53244f834b64009de02c014c73f44d
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-12-29 19:21:51 +0000

    general concepts

---
+++ Nova/meta/concept.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "defines.h"
+
+namespace Nova::meta {
+
+	template<typename ...Ts>
+	concept all_same = sizeof...(Ts) < 2 ||
+		(std::same_as<std::tuple_element_t<0, std::tuple<Ts...>>, Ts> && ...);
+
+	template<typename T, typename ...Ts>
+	concept all_convertable = sizeof...(Ts) < 1 ||
+		(std::convertible_to<Ts, T> && ...);
+
+}
