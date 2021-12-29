87ad97460b200548b574fcfb35221a2c6f6474da
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-12-29 19:28:26 +0000

    flask: dt & eden update

--- Flask/main.cpp
+++ Flask/main.cpp
@@ -1,9 +1,10 @@
 #include "fpch.h"
+#include "test.h"
 
-bool simple_quit(Nova::event::Base& event) {
+bool simple_quit(Nova::event::Handle& event) {
 	if (auto e = event.cast<Nova::event::KeyPress>()) {
-		nova_bark_info("Key: {}", e->key);
-		if (e->key == Nova::input::Key::ESCAPE)
+		nova_bark_info("Key: {} {}", e->key, static_cast<char>(e->key));
+		if (e->key == Nova::input::Key::ESCAPE || ((e->key == Nova::input::Key::F4) && Nova::input::poll(Nova::input::Key::LALT)))
 			return Nova::event::WindowClose().fire();
 	}
 	return false;
@@ -12,12 +13,18 @@ bool simple_quit(Nova::event::Base& event) {
 class Game : public Nova::Application {
 public:
 	Game() : Application("Flask") {
+		test_ep();
+
 		nova_bark_debug("Creating Flask");
-		Nova::event::Register(Nova::event::Type::KeyPress, &simple_quit);
+		Nova::event::dispatcher.subscribe(Nova::event::Type::KeyPress, &simple_quit);
 	}
 
 	void update() {}
-	void render() {}
+	void render() {
+		static constexpr auto req = 1.f / 60.f;
+		if (clock.dt() > req)
+		nova_bark_debug("Frame Time {}", clock.dt());
+	}
 };
 
 #define NOVA_ENTRY Game
---
+++ Flask/test.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+#include "fpch.h"
+
+void test_ep() {
+
+}
