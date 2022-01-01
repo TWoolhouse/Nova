2e8b158e035dbb40c013453b9aba8850e148fd82
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-01-01 13:42:57 +0000

    logging background colours

--- Nova/log/log.cpp
+++ Nova/log/log.cpp
@@ -19,18 +19,20 @@ namespace Nova::bark {
 	}
 
 	void submit(const Level level, const std::string& msg) {
-		static constexpr std::pair<std::string_view, Colour> levels[static_cast<char>(Level::MAX)] = {
-			{"Debug", Colour::Green},
-			{"Trace", Colour::Cyan},
-			{"Info", Colour::White},
-			{"Init", Colour::Blue},
-			{"Term", Colour::Magenta},
-			{"Warn", Colour::Yellow},
-			{"Error", Colour::Red},
-			{"Fatal", Colour::BRed},
+		static constexpr std::pair<std::string_view, std::pair<Colour, Colour>> levels[static_cast<char>(Level::MAX)] = {
+			{"Debug",	{Colour::Green,	Colour::BDefault}},
+			{"Trace",	{Colour::Cyan,	Colour::BDefault}},
+			{"Info",	{Colour::White,	Colour::BDefault}},
+			{"Init",	{Colour::Blue,	Colour::BDefault}},
+			{"Term",	{Colour::Magenta,	Colour::BDefault}},
+			{"Warn",	{Colour::Yellow,	Colour::BDefault}},
+			{"Error",	{Colour::Red,		Colour::BDefault}},
+			{"Fatal",	{Colour::Black,	Colour::BRed}},
 		};
 		const auto& lvl = levels[static_cast<char>(level)];
-		(level < Level::Error ? std::cout : std::cerr) << lvl.second << "[" << lvl.first << "] " << msg << std::endl;
+		(level < Level::Error ? std::cout : std::cerr) << // Stream
+			lvl.second.first << lvl.second.second << // Colours
+			"[" << lvl.first << "] " << msg << std::endl; // Message
 	}
 
 	#ifdef nova_assert
