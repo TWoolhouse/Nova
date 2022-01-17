569627a3e2cdf21d49370ff1e646fbe02363dced
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-01-17 00:23:49 +0000

    nova.h update

--- Nova/nova.h
+++ Nova/nova.h
@@ -3,12 +3,21 @@
 #include "npch.h"
 
 #include "meta/head.h"
+#include "meta/concept.h"
+#include "meta/function.h"
+
 #include "bark/bark.h"
+#include "nvtl/nvtl.h"
+#include "blip/blip.h"
+#include "eden/eden.h"
 
 #include "event/key.h"
 #include "event/mouse.h"
 #include "event/window.h"
 
 #include "input/poll.h"
+
+#include "abyss/abyss.h"
+#include "verglas/verglas.h"
 
 #include "core/application.h"
