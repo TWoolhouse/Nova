a0ed2bb03d9345e84e199916daa96d7f103aea20
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-01-18 23:08:49 +0000

    GLM config defines

--- Nova/mlb/_glm.h
+++ Nova/mlb/_glm.h
@@ -1,14 +1,17 @@
 #include "meta/head.h"
 
-#define GLM_FORCE_INLINE
 #define GLM_FORCE_SWIZZLE
+#define GLM_FORCE_SIZE_T_LENGTH
+#define GLM_FORCE_UNRESTRICTED_GENTYPE
+
+#define GLM_FORCE_INLINE
 
 #ifdef NOVA_DEBUG
 	//#define GLM_FORCE_MESSAGES
 	#define GLM_FORCE_PURE
 #else // !NOVA_DEBUG
 	#define GLM_FORCE_INTRINSICS
-	#define GLM_FORCE_ALIGNED
+	#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #endif // NOVA_DEBUG
 
 #include <glm/fwd.hpp>
