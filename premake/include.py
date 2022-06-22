650631bc0a41dd8ee66384e384ed6d626491382c
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-06-22 17:12:44 +0100

    nova single include file name changes

--- premake/include.py
+++ premake/include.py
@@ -8,9 +8,9 @@ mainfile = Path(sys.argv[1]).absolute().resolve()
 root = mainfile.parent
 
 INCLUDE = "#include"
-def expand(out: TextIO, filename: Path):
+def expand(out: TextIO, filename: Path, level=0):
 	REL_NAME = filename.as_posix()[len(root.as_posix())+1:]
-	out.write(f"// File: {REL_NAME}\n\n")
+	out.write(f"//-----File<{level:>03}>: {REL_NAME:-<59}\\\\\n")
 	print("Import>", filename)
 	with filename.open() as file:
 		for line in file:
@@ -26,7 +26,7 @@ def expand(out: TextIO, filename: Path):
 						if include.is_file() and not include.match((root / "vendor/**/*.h").as_posix()):
 							if include.as_posix() not in done:
 								# print(f"Import: {include}")
-								expand(out, include)
+								expand(out, include, level=level+1)
 							break
 					else:
 						flag = False
@@ -35,8 +35,9 @@ def expand(out: TextIO, filename: Path):
 			elif lline.startswith("#pragma") and (pragma_line := line.strip().split(maxsplit=1)) and len(pragma_line) == 2 and pragma_line[1] == "once":
 				done.add(filename.as_posix())
 				continue
+			# if line.strip():
 			out.write(line)
-	out.write(f"// File: !{REL_NAME}\n")
+	out.write(f"// File<{level:>03}>: !{REL_NAME}\n")
 
 with (root / (sys.argv[2] if len(sys.argv) > 1 else "out.h")).open("w") as file:
 	file.write("#pragma once\n")
