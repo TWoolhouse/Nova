f8d402f325ee5b5f906b0e3d3cdce55f82d82fbf
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-08-14 13:53:01 +0100

    entry point compile time error when missing application

--- Nova/nova_entry.h
+++ Nova/nova_entry.h
@@ -1,14 +1,17 @@
 #pragma once
-#ifndef NOVAPI
+#ifndef NOVA_MAIN
 #include "core/application.h"
-#endif // !NOVAPI
+#endif // !NOVA_MAIN
 
 #ifdef NOVA_ENTRY
 int NOVA_MAIN(int argc, char const* argv[]) {
-	static_assert(std::is_base_of_v<Nova::core::Application, NOVA_ENTRY> && std::is_default_constructible_v<NOVA_ENTRY>,
+	using EntryApp = ::NOVA_ENTRY;
+	static_assert(std::is_base_of_v<Nova::core::Application, EntryApp> && std::is_default_constructible_v<EntryApp>,
 		"NOVA_ENTRY must be a default constructible sub class of application");
-	NOVA_ENTRY app;
+	EntryApp app;
 	app.execute();
 	return 0;
 }
-#endif
+#else // !NOVA_ENTRY
+#error NOVA_ENTRY is not defined before including the entry point.
+#endif // NOVA_ENTRY
