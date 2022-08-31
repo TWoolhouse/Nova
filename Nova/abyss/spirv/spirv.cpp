4430bd3fe7083f92b1d66fb329293c224acf7dd4
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-08-31 22:41:03 +0100

    spirv support for new nvk

--- Nova/abyss/spirv/spirv.cpp
+++ Nova/abyss/spirv/spirv.cpp
@@ -2,21 +2,40 @@
 #include "spirv_compile.h"
 
 #include <shaderc/shaderc.hpp>
-#include <abyss/vulkan/shader.h>
 
 using namespace std::string_literals;
+
+nova_meta_enum_str(shaderc_compilation_status, status) {
+	using Status = decltype(status);
+	switch (status) {
+		case Status::shaderc_compilation_status_success:	return "Sucess";
+		case Status::shaderc_compilation_status_invalid_stage:	return "Invalid Stage";
+		case Status::shaderc_compilation_status_compilation_error:	return "Compilation Error";
+		case Status::shaderc_compilation_status_internal_error:	return "Internal Error";
+		case Status::shaderc_compilation_status_null_result_object:	return "Null Result Object";
+		case Status::shaderc_compilation_status_invalid_assembly:	return "Invalid Assembly";
+		case Status::shaderc_compilation_status_validation_error:	return "Validation Error";
+		case Status::shaderc_compilation_status_transformation_error:	return "Transformation Error";
+		case Status::shaderc_compilation_status_configuration_error:	return "Configuration Error";
+	default:
+		nova_bark_warn("Unknown Case []: {}", static_cast<std::underlying_type_t<decltype(status)>>(status));
+		return "Unknown";
+	}
+}
 
 namespace Nova::abyss::spirv {
 
 	const std::filesystem::path shader_src{ "shader" };
 	const std::filesystem::path shader_bin{ "shader/bin" };
 
-	constexpr shaderc_shader_kind shader_type(const vkn::Shader::Type type) {
-		using namespace vkn;
-		switch (type) {
-			case Shader::Type::Vertex:	return shaderc_shader_kind::shaderc_glsl_default_vertex_shader;
-			case Shader::Type::Fragment:	return shaderc_shader_kind::shaderc_glsl_default_fragment_shader;
+	constexpr shaderc_shader_kind shader_type(const Shader::Stage stage) {
+		using namespace nvk;
+		using Stage = decltype(stage);
+		switch (stage) {
+			case Stage::Vertex:	return shaderc_shader_kind::shaderc_glsl_default_vertex_shader;
+			case Stage::Fragment:	return shaderc_shader_kind::shaderc_glsl_default_fragment_shader;
 			default:
+				nova_bark_warn("Unahndled Case [Shader Stage]: {}", stage);
 				return shaderc_shader_kind::shaderc_glsl_infer_from_source;
 		}
 	}
@@ -40,7 +59,7 @@ namespace Nova::abyss::spirv {
 		return buffer;
 	}
 
-	Binary compile(const std::filesystem::path& file_path, const vkn::Shader::Type type) {
+	Binary compile(const std::filesystem::path& file_path, const nvk::Shader::Stage stage) {
 		const auto shader = read_src_file(file_path);
 		shaderc::Compiler compiler;
 		shaderc::CompileOptions options;
@@ -53,20 +72,21 @@ namespace Nova::abyss::spirv {
 		}
 		const auto spv = compiler.CompileGlslToSpv(
 			shader,
-			shader_type(type),
+			shader_type(stage),
 			file_path.generic_string().c_str(),
 			options
 		);
-		if (spv.GetNumErrors()) {
-			nova_bark_error("Shader Compiliation[{}]<{}> {}", static_cast<std::underlying_type_t<decltype(spv.GetCompilationStatus())>>(spv.GetCompilationStatus()), file_path.generic_string(), spv.GetErrorMessage());
-			throw std::runtime_error("Shader Compilation Error");
+		if (spv.GetCompilationStatus() != shaderc_compilation_status::shaderc_compilation_status_success) {
+			nova_bark_error("Shader Compiliation[{}]<{}> {}", spv.GetCompilationStatus(), file_path.generic_string(), spv.GetErrorMessage());
+			throw std::runtime_error("Shader Compilation Error");		
 		}
+		nova_assert(std::distance(spv.cbegin(), spv.cend()) > 0, "Shader Code must have a length greater than 0!");
 		return { spv.cbegin(), spv.cend() };
 	}
 
-	Binary compile(const std::filesystem::path& ifile, const std::filesystem::path& ofile, const vkn::Shader::Type type) {
+	Binary compile(const std::filesystem::path& ifile, const std::filesystem::path& ofile, const nvk::Shader::Stage stage) {
 		nova_bark_info("Compiling Shader: {}", ifile.generic_string());
-		auto binary = compile(ifile, type);
+		auto binary = compile(ifile, stage);
 		std::filesystem::create_directories(ofile.parent_path());
 		std::ofstream file{ ofile, std::ios::out | std::ios::binary };
 		if (!file) throw std::invalid_argument("Unable to Open File:"s + ofile.generic_string());
@@ -74,20 +94,12 @@ namespace Nova::abyss::spirv {
 		return binary;
 	}
 
-	Binary file(const std::filesystem::path& shader_file) {
+	Binary file(const std::filesystem::path& shader_file, const nvk::Shader::Stage stage) {
 		nova_bark_debug("Loading Shader: {}", shader_file.generic_string());
 		const auto path_spv = shader_bin / shader_file;
 		if (std::filesystem::exists(path_spv))
 			return read_spv_file(path_spv);
-		return spirv::compile(shader_src / shader_file, path_spv, vkn::Shader::Type::Default);
-	}
-
-	Binary file(const std::filesystem::path& shader_file, const vkn::Shader::Type type) {
-		nova_bark_debug("Loading Shader: {}", shader_file.generic_string());
-		const auto path_spv = shader_bin / shader_file;
-		if (std::filesystem::exists(path_spv))
-			return read_spv_file(path_spv);
-		return spirv::compile(shader_src / shader_file, path_spv, type);
+		return spirv::compile(shader_src / shader_file, path_spv, stage);
 	}
 
 }
--- Nova/abyss/spirv/spirv_compile.h
+++ Nova/abyss/spirv/spirv_compile.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "meta/head.h"
 #include "spirv.h"
-#include "abyss/vulkan/shader.h"
+
+#include "abyss/shader.h"
 
 namespace Nova::abyss::spirv {
 
-	Binary compile(const std::filesystem::path& file, const vkn::Shader::Type type);
+	Binary compile(const std::filesystem::path& file, const Shader::Stage stage);
 	Binary compile(const std::filesystem::path& ifile, const std::filesystem::path& ofile);
 
 	Binary file(const std::filesystem::path& shader_file);
-	Binary file(const std::filesystem::path& shader_file, const vkn::Shader::Type type);
+	Binary file(const std::filesystem::path& shader_file, const Shader::Stage type);
 
 }
