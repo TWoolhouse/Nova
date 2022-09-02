f3a2ac8b6a0712a94d18fe3537d610fb4ab11217
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-09-02 17:05:52 +0100

    premake msvc extra warnings

--- premake/config.lua
+++ premake/config.lua
@@ -4,6 +4,16 @@ filter "configurations:Debug"
 	symbols "Full"
 	intrinsics "off"
 
+filter { "configurations:Debug", "action:vs*" }
+	buildoptions {
+		"/w35240", -- Attribute is ignored in this position
+		"/w34100", -- Unreference formal parameter
+		"/w34834", -- Discarding nodiscard
+		"/w34702", -- Code unreachable
+		"/w34189", -- Local variable declared but not used
+		"/w34946", -- Reinterpret cast on related classes
+	}
+
 filter "configurations:Release"
 	defines { "NDEBUG" }
 	removedefines { "DEBUG", "_DEBUG" }
