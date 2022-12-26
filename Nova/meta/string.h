5789559eb34039992b29e9a77aa2d1c000481478
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-12-26 14:16:59 +0000

    string join with delimiter

---
+++ Nova/meta/string.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "head.h"
+
+namespace Nova::meta::str {
+
+    template<std::ranges::forward_range Rng>
+    std::string join(Rng&& rng, const std::string& delimiter = ", ") {
+        std::stringstream stream;
+        if (auto it = std::ranges::begin(rng), e = std::ranges::end(rng); it != e) {
+            stream << *it++;
+            for (; it != e; ++it) stream << delimiter << *it;
+        }
+        return stream.str();
+    }
+
+}
