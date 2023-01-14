6226429d33481c440b24dfadc2638c157f663d52
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-01-14 01:19:46 +0000

    spaces -> tabs

--- Nova/meta/string.h
+++ Nova/meta/string.h
@@ -3,14 +3,14 @@
 
 namespace Nova::meta::str {
 
-    template<std::ranges::forward_range Rng>
-    std::string join(Rng&& rng, const std::string& delimiter = ", ") {
-        std::stringstream stream;
-        if (auto it = std::ranges::begin(rng), e = std::ranges::end(rng); it != e) {
-            stream << *it++;
-            for (; it != e; ++it) stream << delimiter << *it;
-        }
-        return stream.str();
-    }
+	template<std::ranges::forward_range Rng>
+	std::string join(Rng&& rng, const std::string& delimiter = ", ") {
+		std::stringstream stream;
+		if (auto it = std::ranges::begin(rng), e = std::ranges::end(rng); it != e) {
+			stream << *it++;
+			for (; it != e; ++it) stream << delimiter << *it;
+		}
+		return stream.str();
+	}
 
 }
