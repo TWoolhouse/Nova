04d23933f59cfed9ab1034035cc477d473971447
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2023-01-22 16:12:12 +0000

    window size type fixed

--- Nova/core/window.h
+++ Nova/core/window.h
@@ -5,22 +5,21 @@ namespace Nova::core {
 
 	struct Window {
 	public:
-		Window(const unsigned int width, const unsigned int height) : m_width(width), m_height(height) {}
+		Window(const uint32_t width, const uint32_t height) : m_width(width), m_height(height) {}
 		~Window() {}
 
 		NODISCARD constexpr inline const auto& width() const noexcept { return m_width; }
 		NODISCARD constexpr inline const auto& height() const noexcept { return m_height; }
-		NODISCARD constexpr inline const std::pair<const unsigned int&, const unsigned int&> size() const noexcept { return { m_width, m_height }; }
-
+		NODISCARD constexpr inline const std::pair<const uint32_t&, const uint32_t&> size() const noexcept { return { m_width, m_height }; }
 
 		// Resizes the window to the requested values.
-		void resize(const unsigned int width, const unsigned int height);
+		void resize(const uint32_t width, const uint32_t height);
 	protected:
-		unsigned int m_width, m_height;
+		uint32_t m_width, m_height;
 
 		friend class Application;
 		// Sets the size values, does not perform a resize.
-		inline void size(const unsigned int width, const unsigned int height) noexcept {
+		inline void size(const uint32_t width, const uint32_t height) noexcept {
 			m_width = width; m_height = height;
 		}
 	};
