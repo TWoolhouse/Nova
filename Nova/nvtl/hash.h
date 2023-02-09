3f3c6d2f0a6ce9bfe461b6d61b96d21f8024e40d
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2023-02-09 15:46:13 +0000

    hash_all

--- Nova/nvtl/hash.h
+++ Nova/nvtl/hash.h
@@ -4,12 +4,12 @@
 namespace Nova::nvtl {
 
 	template<typename T>
-	size_t hash(const T& object) noexcept {
-		return std::hash<T>{}(object);
+	constexpr std::size_t hash(T&& object) {
+		return std::hash<std::remove_cvref_t<T>>{}(std::forward<T>(object));
 	}
 
 	// https://www.boost.org/doc/libs/1_55_0/doc/html/hash/combine.html
-	size_t hash_combine(size_t lhs, size_t rhs) {
+	constexpr std::size_t hash_combine(size_t lhs, size_t rhs) noexcept {
 		if constexpr (sizeof(size_t) >= 8) {
 			lhs ^= rhs + 0x517cc1b727220a95 + (lhs << 6) + (lhs >> 2);
 		}
@@ -17,6 +17,22 @@ namespace Nova::nvtl {
 			lhs ^= rhs + 0x9e3779b9 + (lhs << 6) + (lhs >> 2);
 		}
 		return lhs;
+	}
+
+	namespace __I {
+		template<typename T, typename ...Args>
+		constexpr auto hash_all(std::size_t hash, T&& arg, Args&& ...args) {
+			if constexpr (sizeof...(Args) <= 0) {
+				return nvtl::hash_combine(hash, nvtl::hash(std::forward<T>(arg)));
+			} else {
+				return hash_all(nvtl::hash_combine(hash, nvtl::hash(std::forward<T>(arg))), std::forward<Args>(args)...);
+			}
+		}
+	} // namespace __I
+
+	template<typename ...Args> requires (sizeof...(Args) > 0)
+	constexpr std::size_t hash_all(Args&& ...args) {
+		return __I::hash_all(0, std::forward<Args>(args)...);
 	}
 
 } // namespace Nova::nvtl
